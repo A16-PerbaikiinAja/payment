a4e6b46d2c898113f3ee67150805e2f61da42ec1
ghina2703 <ghina2703@gmail.com>
2025-04-09 01:23:45 +0700

    [RED] Add skeleton for PaymentMethodService

---
+++ src/main/java/id/ac/ui/cs/advprog/payment/service/PaymentMethodService.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.payment.service;
+
+import id.ac.ui.cs.advprog.payment.dto.paymentmethod.PaymentMethodDTO;
+import id.ac.ui.cs.advprog.payment.dto.paymentmethod.PaymentMethodRegisterDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.Map;
+import java.util.UUID;
+
+public interface PaymentMethodService {
+}
