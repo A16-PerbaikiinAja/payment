d5f1a3b6915df92762598a4e0453c5bb4f5c5d12
ghina2703 <ghina2703@gmail.com>
2025-04-08 19:06:34 +0700

    [GREEN] Implement UserRegisterDTO

--- src/test/java/id/ac/ui/cs/advprog/payment/dto/user/UserRegisterDTO.java
+++ src/test/java/id/ac/ui/cs/advprog/payment/dto/user/UserRegisterDTO.java
@@ -6,4 +6,11 @@ import lombok.Generated;
 @Generated
 @Data
 public class UserRegisterDTO {
+    private String email;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private String address;
+    private String role;
+    private String profileImage;
 }
