87323277774b6205f7fd67222b5c609e12c4878e
ghina2703 <ghina2703@gmail.com>
2025-04-08 14:16:57 +0700

    [RED] Add skeleton for COD unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/payment/model/CODTest.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.payment.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CODTest {
+
+    private COD cod;
+    private COD codCopy;
+    private PaymentMethod paymentMethod;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testConstructorWithPaymentMethod() {
+    }
+
+    @Test
+    void testDefaultConstructor() {
+    }
+
+    @Test
+    void testGettersAndSetters() {
+    }
+
+    @Test
+    void testInheritanceFromPaymentMethod() {
+    }
+
+    @Test
+    void testFieldValidation() {
+    }
+}
