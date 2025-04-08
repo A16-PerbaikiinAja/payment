aa47fe0fbb1fda6bc068260455d43e7c3822f404
ghina2703 <ghina2703@gmail.com>
2025-04-08 17:16:32 +0700

    [RED] Add skeleton for PaymentMethodRegisterDTO

---
+++ src/test/java/id/ac/ui/cs/advprog/payment/dto/PaymentMethodRegisterDTO.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.payment.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Generated;
+
+import java.math.BigDecimal;
+
+@Generated
+@Data
+public class PaymentMethodRegisterDTO {
+}
