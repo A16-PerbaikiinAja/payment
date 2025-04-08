69e15d34aa38b21f3700cbd3bd007f0f1d612a21
ghina2703 <ghina2703@gmail.com>
2025-04-08 14:24:41 +0700

    [GREEN] Implement User entity class

--- src/main/java/id/ac/ui/cs/advprog/payment/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/model/User.java
@@ -12,4 +12,30 @@ import java.util.UUID;
 @Generated
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "role", nullable = false)
+    private String role;
+    @Column(name = "profile_image")
+    private String profileImage;
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+    @Column(name = "last_login")
+    private Timestamp lastLogin;
 }
