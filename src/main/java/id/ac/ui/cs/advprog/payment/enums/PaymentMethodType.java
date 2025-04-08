0517e2af683d01da04c37981a5c94c3e55f858de
ghina2703 <ghina2703@gmail.com>
2025-04-09 01:58:30 +0700

    [GREEN] Implement PaymentMethodType

--- src/main/java/id/ac/ui/cs/advprog/payment/enums/PaymentMethodType.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/enums/PaymentMethodType.java
@@ -1,7 +1,17 @@
 package id.ac.ui.cs.advprog.payment.enums;
 
 public enum PaymentMethodType {
+    COD,
+    BANK_TRANSFER,
+    E_WALLET,
+    INVALID;
 
     public static PaymentMethodType fromString(String type) {
+        return switch (type.toUpperCase()) {
+            case "COD" -> COD;
+            case "BANK_TRANSFER" -> BANK_TRANSFER;
+            case "E_WALLET" -> E_WALLET;
+            default -> INVALID;
+        };
     }
 }
