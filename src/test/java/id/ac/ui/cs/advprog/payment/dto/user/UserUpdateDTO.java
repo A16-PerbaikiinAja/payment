514677e5e954bb49f15845e2a98d38a6c842850e
ghina2703 <ghina2703@gmail.com>
2025-04-08 19:08:12 +0700

    [GREEN] Implement UserUpdateDTO

--- src/test/java/id/ac/ui/cs/advprog/payment/dto/user/UserUpdateDTO.java
+++ src/test/java/id/ac/ui/cs/advprog/payment/dto/user/UserUpdateDTO.java
@@ -6,4 +6,11 @@ import lombok.Generated;
 @Generated
 @Data
 public class UserUpdateDTO {
+    private String email;
+    private String password;
+    private String full_name;
+    private String phoneNumber;
+    private String address;
+    private String role;
+    private String profileImage;
 }
