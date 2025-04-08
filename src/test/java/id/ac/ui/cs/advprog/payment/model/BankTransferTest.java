535e1c0d37a5ce5afa8a3cff12694d8c0bf56935
ghina2703 <ghina2703@gmail.com>
2025-04-08 13:43:20 +0700

    [RED] Add skeleton for BankTransfer unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/payment/model/BankTransferTest.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.payment.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BankTransferTest {
+
+    private BankTransfer bankTransfer;
+    private PaymentMethod paymentMethod;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testConstructorWithPaymentMethod() {
+    }
+
+    @Test
+    void testDefaultConstructor() {
+    }
+
+    @Test
+    void testGettersAndSetters() {
+    }
+
+    @Test
+    void testInheritanceFromPaymentMethod() {
+    }
+
+    @Test
+    void testSetterAndGetterMethods() {
+    }
+}
