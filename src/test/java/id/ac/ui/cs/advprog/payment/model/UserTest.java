ef3f8d69f432f866b4413e795d702361a8edf3a6
ghina2703 <ghina2703@gmail.com>
2025-04-08 14:10:59 +0700

    [RED] Add skeleton for User unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/payment/model/UserTest.java
@@ -0,0 +1,37 @@
+package id.ac.ui.cs.advprog.payment.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+class UserTest {
+
+    private User user;
+    private User userCopy;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testConstructorWithAnotherUser() {
+    }
+
+    @Test
+    void testDefaultConstructor() {
+    }
+
+    @Test
+    void testGettersAndSetters() {
+    }
+
+    @Test
+    void testIdGeneration() {
+    }
+
+    @Test
+    void testFieldValidation() {
+    }
+}
