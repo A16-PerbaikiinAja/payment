054366ed0685d4e937de5bcc74eea6122c7499ab
ghina2703 <ghina2703@gmail.com>
2025-04-08 14:27:46 +0700

    [GREEN] Implement PaymentMethod entity class

--- src/main/java/id/ac/ui/cs/advprog/payment/model/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/model/PaymentMethod.java
@@ -14,8 +14,40 @@ import java.util.UUID;
 @Generated
 @Data
 public class PaymentMethod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "processing_fee")
+    private BigDecimal processingFee;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updatedAt;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "created_by", referencedColumnName = "id", nullable = false)
+    private User createdBy;
 
     public PaymentMethod(PaymentMethod other) {
+        this.name = other.name;
+        this.description = other.description;
+        this.processingFee = other.processingFee;
+        this.createdAt = other.createdAt;
+        this.updatedAt = other.updatedAt;
+        this.createdBy = other.createdBy;
     }
 
     public PaymentMethod() {}
