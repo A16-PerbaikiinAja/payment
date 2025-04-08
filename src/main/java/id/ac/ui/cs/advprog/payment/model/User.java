80d1f571200bdbacafa7e6dae8711630aabc58cc
ghina2703 <ghina2703@gmail.com>
2025-04-08 14:23:58 +0700

    [RED] Add skeleton for User entity class

---
+++ src/main/java/id/ac/ui/cs/advprog/payment/model/User.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.payment.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Generated
+@Data
+public class User {
+}
