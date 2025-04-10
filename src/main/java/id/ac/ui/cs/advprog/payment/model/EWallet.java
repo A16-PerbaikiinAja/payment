9479a41536bc890f651eea40c3896d6d206d2dd9
ghina2703 <ghina2703@gmail.com>
2025-04-10 19:54:10 +0700

    [REFACTOR] Remove unused constructor parameter and unnecessary import in EWallet class

--- src/main/java/id/ac/ui/cs/advprog/payment/model/EWallet.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/model/EWallet.java
@@ -1,6 +1,9 @@
 package id.ac.ui.cs.advprog.payment.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -21,8 +24,4 @@ public class EWallet extends PaymentMethod {
 
     @Column(name = "instructions", columnDefinition = "TEXT")
     private String instructions;
-
-    public EWallet(PaymentMethod paymentMethod) {
-        super();
-    }
 }
