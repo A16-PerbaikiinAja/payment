822a05b9854ffa7ad8be95ebec6e8beab4ebc617
ghina2703 <ghina2703@gmail.com>
2025-04-10 20:06:00 +0700

    [REFACTOR] Removed unnecessary imports in PaymentMethodController

--- src/main/java/id/ac/ui/cs/advprog/payment/controller/PaymentMethodController.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/controller/PaymentMethodController.java
@@ -2,7 +2,15 @@ package id.ac.ui.cs.advprog.payment.controller;
 
 import id.ac.ui.cs.advprog.payment.dto.paymentmethod.PaymentMethodRegisterDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
