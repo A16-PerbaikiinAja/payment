90b92f2b24526900788d9585e504cd2974dcd687
ghina2703 <ghina2703@gmail.com>
2025-04-10 19:56:09 +0700

    [REFACTOR] Remove unused constructor parameter and unnecessary import in COD class

--- src/main/java/id/ac/ui/cs/advprog/payment/model/COD.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/model/COD.java
@@ -1,6 +1,9 @@
 package id.ac.ui.cs.advprog.payment.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -18,8 +21,4 @@ public class COD extends PaymentMethod {
 
     @Column(name = "instructions", columnDefinition = "TEXT")
     private String instructions;
-
-    public COD(PaymentMethod paymentMethod) {
-        super();
-    }
 }
