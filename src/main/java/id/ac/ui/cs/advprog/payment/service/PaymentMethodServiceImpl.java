2409836565d585b8ed1c23c026f9d02070db0def
ghina2703 <ghina2703@gmail.com>
2025-04-10 21:04:35 +0700

    [REFACTOR] Refactor PaymentMethodServiceImpl for deletePaymentMethod

--- src/main/java/id/ac/ui/cs/advprog/payment/service/PaymentMethodServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/service/PaymentMethodServiceImpl.java
@@ -15,17 +15,20 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 @Service
 public class PaymentMethodServiceImpl implements PaymentMethodService {
@@ -134,8 +137,8 @@ public class PaymentMethodServiceImpl implements PaymentMethodService {
     }
 
     @Override
-    public Map<String, Object> deletePaymentMethod(String id) {
-        return null;
+    public ArrayList<Object> deletePaymentMethod(String id) {
+        return new ArrayList<>();
     }
 
     private PaymentMethodDTO convertToDTO(PaymentMethod method) {
