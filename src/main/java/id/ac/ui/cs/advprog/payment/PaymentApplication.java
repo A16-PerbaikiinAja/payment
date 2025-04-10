cda105403928a2a321818d470fddddd87c04e374
ghina2703 <ghina2703@gmail.com>
2025-04-10 20:38:31 +0700

    [REFACTOR] Made PaymentApplication class a utility class by adding a private constructor

--- src/main/java/id/ac/ui/cs/advprog/payment/PaymentApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/PaymentApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentApplication {
 
+    private PaymentApplication() {
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
-
 }
