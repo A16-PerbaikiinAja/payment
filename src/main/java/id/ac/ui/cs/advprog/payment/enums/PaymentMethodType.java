e5870fc3373746fbf28c177b44b97b75e07d5a03
ghina2703 <ghina2703@gmail.com>
2025-04-10 20:01:42 +0700

    [REFACTOR] Use Locale.ROOT for case conversions in PaymentMethodType enums

--- src/main/java/id/ac/ui/cs/advprog/payment/enums/PaymentMethodType.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/enums/PaymentMethodType.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.payment.enums;
 
+import java.util.Locale;
+
 public enum PaymentMethodType {
     COD,
     BANK_TRANSFER,
@@ -7,7 +9,7 @@ public enum PaymentMethodType {
     INVALID;
 
     public static PaymentMethodType fromString(String type) {
-        return switch (type.toUpperCase()) {
+        return switch (type.toUpperCase(Locale.ROOT)) {
             case "COD" -> COD;
             case "BANK_TRANSFER" -> BANK_TRANSFER;
             case "E_WALLET" -> E_WALLET;
