482e8ad4e35fcabde6a676204241e4e6d0cf2ceb
ghina2703 <ghina2703@gmail.com>
2025-05-08 23:58:27 +0700

    [RED] Add skeleton for PaymentMethodType and fromString method

---
+++ src/main/java/id/ac/ui/cs/advprog/payment/constant/PaymentMethodType.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.payment.constant;
+
+public enum PaymentMethodType {
+    COD,
+    BANK_TRANSFER,
+    E_WALLET,
+    INVALID;
+
+    public static PaymentMethodType fromString(String type) {
+        };
+    }
+}
