f266535967f423957e8859142cb451997e27231c
ghina2703 <ghina2703@gmail.com>
2025-05-09 00:01:03 +0700

    [RED] Add skeleton for Status

--- src/main/java/id/ac/ui/cs/advprog/payment/constant/PaymentMethodType.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/constant/PaymentMethodType.java
@@ -7,6 +7,11 @@ public enum PaymentMethodType {
     INVALID;
 
     public static PaymentMethodType fromString(String type) {
+        return switch (type.toUpperCase()) {
+            case "COD" -> COD;
+            case "BANK_TRANSFER" -> BANK_TRANSFER;
+            case "E_WALLET" -> E_WALLET;
+            default -> INVALID;
         };
     }
 }
