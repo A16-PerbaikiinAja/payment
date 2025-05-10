887a39f6f2372e3b2182ec2ac75694342f6e05f7
ghina2703 <ghina2703@gmail.com>
2025-05-10 16:37:16 +0700

    [FIX] Deployment

--- src/main/java/id/ac/ui/cs/advprog/payment/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import id.ac.ui.cs.advprog.payment.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
